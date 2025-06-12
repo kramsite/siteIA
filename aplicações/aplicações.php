91c6222236d4c3d2c380e0e81b481b29d440a89e
foag <cursotecnicorrkm@gmail.com>
2025-06-12 08:47:19 -0400

    dds
    sddd

--- aplicações/aplicações.php
+++ aplicações/aplicações.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Tipos de Redes Neurais</title>
-  <link rel="stylesheet" href="tipos.css">
+  <link rel="stylesheet" href="aplicações.css">
 </head>
 <body>
 
