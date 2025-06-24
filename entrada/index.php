352d66da7f5470683e1b4c3a9244bdae0a4a70af
foag <cursotecnicorrkm@gmail.com>
2025-06-24 07:47:23 -0400

    .

--- entrada/index.php
+++ entrada/index.php
@@ -102,7 +102,7 @@
         <div class="login-container">
             <h2>Bem-vindo de volta</h2>
 
-            <form action="processa_login.php" method="post">
+            <form action="../oque/oque.php" method="post">
                 <input type="text" name="nome" placeholder="Usuário" required autocomplete="username">
                 <input type="password" name="senha" placeholder="Senha" required autocomplete="current-password">
                 <p class="cadastro-link"><a href="../cadastro/cadastro.php">Cadastre-se</a></p>
